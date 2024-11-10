180855ca36e4c7d11e79ec8431d9b09ac8bda76e
Alexander Morgunov <morgunov.02@list.ru>
2024-11-10 17:51:06 +0300

    Wiring

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+    private Needle7 needlel7;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+
+        return ", в яйце иголка " + needlel7.toString();
+    }
+    @Autowired
+    public void setNeedlel7(Needle7 needlel7) {
+        this.needlel7 = needlel7;
     }
 }
